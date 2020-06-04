30cb0259feefad797de723740d8ca2405bbd14f6
jon-ht <jonathan.huteau15@gmail.com>
2020-06-04 21:08:00 +0200

    Add composer deps + revamps src folder

---
+++ src/Infrastructure/Kernel.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $confDir = $this->getProjectDir() . '/config';
+        $container->import($confDir.'/{packages}/*.yaml');
+        $container->import($confDir.'/{packages}/' . $this->environment . '/*.yaml');
+        $container->import($confDir.'/{services}.yaml');
+        $container->import($confDir.'/{services}_' . $this->environment . '.yaml');
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $confDir = $this->getProjectDir() . '/config';
+        $routes->import($confDir.'/{routes}/' . $this->environment . '/*.yaml');
+        $routes->import($confDir.'/{routes}/*.yaml');
+        $routes->import($confDir.'/{routes}.yaml');
+    }
+}
